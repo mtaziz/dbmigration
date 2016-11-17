8c32255641594531ed348897a6417e591a01f8dd
omarnixx <umarashfaq09@gmail.com>
2016-11-17 13:58:29 -0500

    Update migrator.py

--- migrator.py
+++ migrator.py
@@ -29,11 +29,11 @@ for obj in file:
         print("Adding :", key1, key2, key3, key4,key5,key6)
         table.put_item(
            Item={
-               '_id': key1,
-               'k': key2,
-               't': key3,
-               'ts': key4,
-               'c': key5,
-               'i': key6
+               'key1': key1,
+               'key2': key2,
+               'key3': key3,
+               'key4': key4,
+               'key5': key5,
+               'key6': key6
             }
         )
